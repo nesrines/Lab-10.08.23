7322ceb8c898284033724270403f083f55371bd1
nesrines <nasrinfa@code.edu.az>
2023-08-11 13:58:02 +0400

    done

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace WebApplication1.Controllers;
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        List<object> objects = new()
+        {
+            new { id = 1, name = "Test" },
+            new { id = 1, name = "Test" },
+            new { id = 1, name = "Test" }
+        };
+        ViewBag.objects = objects;
+        return View();
+    }
+}
---
+++ WebApplication1/Program.cs
@@ -0,0 +1,5 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+var app = builder.Build();
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}");
+app.Run();
